df759ab5d731b6f2d6bf3993801aaa7236ec5528
Mahdi777-Hex <mahdi.1111lahbali@gmail.com>
2024-12-10 22:13:37 +0100

    Add files via upload

---
+++ Bandits.js
@@ -0,0 +1,12 @@
+loopy() 
+ function loopy() { 
+ var sWord ="@SH4ko1" 
+ while (sWord != "Bandits.com") {// Pass
+ sWord = prompt("⇰ 𝘌𝘯𝘵𝘦𝘳 𝘛𝘩𝘦 𝘒𝘦𝘺 :")  
+ } 
+ 
+//confirm("ok");
+
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 𝐁𝐘 ⪼ 𝐁𝐀𝐍𝐃𝐈𝐓𝐒  ");
+
+ } 
